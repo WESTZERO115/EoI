07a06739fbe1f64c2f155b40d25e3d40355b2cf8
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2022-03-21 22:40:35 +0900

    Update and rename A.py to ex03.py

---
+++ HyeRin/TIL/220321/ex03.py
@@ -0,0 +1,9 @@
+#사용자로부터 두 개의 정수를 입력받아서 두 수의 합을 구하는 프로그램
+
+x = int(input("첫 번째 정수 : "))
+y = int(input("두 번째 정수 : "))
+
+sum = x + y
+diff = x - y
+print("두 수의 합은 : ", sum)
+print("두 수의 차는 : ", diff)
