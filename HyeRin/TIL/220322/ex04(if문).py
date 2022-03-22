27b73a650faf3c60620aad410292b5a55696646a
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2022-03-23 00:11:25 +0900

    Create ex04(if문).py

---
+++ HyeRin/TIL/220322/ex04(if문).py
@@ -0,0 +1,15 @@
+# 조건문
+
+score = int(input("점수를 입력하세요 : "))
+if score >= 60 :
+    print("축하드립니다. 합격입니다.")
+else :
+    print("아쉽게도 불합격입니다.")
+
+# 부울(bool) 변수 알아보기
+flag = True
+
+if flag:
+    print("참입니다.")
+else :
+    print("거짓입니다.")
