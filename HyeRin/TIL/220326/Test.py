7f4dc975523eb51878f38455807b1891aa341be4
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2022-03-27 00:10:12 +0900

    Update Test.py

--- HyeRin/TIL/220326/Test.py
+++ HyeRin/TIL/220326/Test.py
@@ -8,10 +8,37 @@
 5) 키워드를 사용하면 안된다(if, for 등)
 '''
 
-
 a=1
 A=2
 A1=3
 _b=4
 #2b=4
 print(a, A, A1)
+
+#값 교환
+a, b=10,20
+print(a,b)
+a, b=b, a
+print(a, b)
+
+#변수 타입
+a=12345
+print(a)
+print(type(a))
+b=1234.1234556
+print(b)
+print(type(b))
+a="student"
+print(a)
+print(type(a))
+
+#출력방식
+print("number")
+a,b,c=1,2,3
+print(a,b,c)
+print("number : ",a,b,c)
+print(a,b,c, sep=', ') #a,b,c 사이에 콤마넣고 띄어쓰기
+print(a,b,c, sep='\n')
+print(a, end=' ')
+print(b, end=' ')
+print(c)
