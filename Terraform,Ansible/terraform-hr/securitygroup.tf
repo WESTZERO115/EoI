a252f4afc3eaca894ce79164e389fb27ccdcb9b5
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2022-06-08 01:14:14 +0900

    Update securitygroup.tf

--- Terraform,Ansible/terraform-hr/securitygroup.tf
+++ Terraform,Ansible/terraform-hr/securitygroup.tf
@@ -27,3 +27,53 @@ resource "aws_security_group" "security" {
     SyncToken  = data.aws_ip_ranges.european_ec2.sync_token
   }
 }
+
+resource "aws_security_group" "ecs-securitygroup" {
+  vpc_id      = aws_vpc.main.id
+  name        = "ecs"
+  description = "security group for ecs"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 3000
+    to_port         = 3000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.myapp-elb-securitygroup.id]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "ecs"
+  }
+}
+
+resource "aws_security_group" "myapp-elb-securitygroup" {
+  vpc_id      = aws_vpc.main.id
+  name        = "myapp-elb"
+  description = "security group for ecs"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "myapp-elb"
+  }
+}
