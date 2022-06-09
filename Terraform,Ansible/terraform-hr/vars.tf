32d2c94503ac22ee9417e569b05cbbadb55d42ef
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2022-06-09 16:43:07 +0900

    Update vars.tf

--- Terraform,Ansible/terraform-hr/vars.tf
+++ Terraform,Ansible/terraform-hr/vars.tf
@@ -2,6 +2,13 @@ variable "AWS_REGION" {
   default = "us-west-2"
 }
 
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
 
 variable "AMIS" {
   type = map(string)
@@ -10,3 +17,4 @@ variable "AMIS" {
   }
 
 }
+
