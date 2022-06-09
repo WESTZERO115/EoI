0866bc3c213d270b3ea20cfbfa653bd9361d1e5f
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2022-06-09 16:42:33 +0900

    Update securitygroup.tf

--- Terraform,Ansible/terraform-hr/securitygroup.tf
+++ Terraform,Ansible/terraform-hr/securitygroup.tf
@@ -1,37 +1,7 @@
-data "aws_ip_ranges" "european_ec2" {
-  regions  = ["us-west-2", "us-west-1"]
-  services = ["ec2"]
-}
-
-resource "aws_security_group" "security" {
-  name        = "security"
+resource "aws_security_group" "allow-ssh" {
   vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp" 
-    cidr_blocks      = slice(data.aws_ip_ranges.european_ec2.cidr_blocks, 0, 50)
-  }
-
-  egress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp" 
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    CreateDate = data.aws_ip_ranges.european_ec2.create_date
-    SyncToken  = data.aws_ip_ranges.european_ec2.sync_token
-  }
-}
-
-resource "aws_security_group" "ecs-securitygroup" {
-  vpc_id      = aws_vpc.main.id
-  name        = "ecs"
-  description = "security group for ecs"
+  name        = "allow-ssh"
+  description = "security group that allows ssh and all egress traffic"
   egress {
     from_port   = 0
     to_port     = 0
@@ -40,40 +10,13 @@ resource "aws_security_group" "ecs-securitygroup" {
   }
 
   ingress {
-    from_port       = 3000
-    to_port         = 3000
-    protocol        = "tcp"
-    security_groups = [aws_security_group.myapp-elb-securitygroup.id]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "ecs"
-  }
-}
-
-resource "aws_security_group" "myapp-elb-securitygroup" {
-  vpc_id      = aws_vpc.main.id
-  name        = "myapp-elb"
-  description = "security group for ecs"
-  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   tags = {
-    Name = "myapp-elb"
+    Name = "allow-ssh"
   }
 }
+
