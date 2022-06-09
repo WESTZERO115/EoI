ee558a6e0f30513d8702d3fa133bf853e5274da1
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2022-06-09 16:39:48 +0900

    Update instance.tf

--- Terraform,Ansible/terraform-hr/instance.tf
+++ Terraform,Ansible/terraform-hr/instance.tf
@@ -1,4 +1,11 @@
 resource "aws_instance" "example" {
   ami = var.AMIS[var.AWS_REGION]
   instance_type = "t2.micro"
+  
+  key_name = aws_key_pair.mykeypair.key_name
+
+  subnet_id = aws_subnet.main-public-1.id
+
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+
 }
