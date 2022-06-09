c2fd7d0f69e7223333767e9d466308b503f98c83
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2022-06-09 14:52:17 +0900

    Update vpc.tf

--- Terraform,Ansible/terraform-hr/vpc.tf
+++ Terraform,Ansible/terraform-hr/vpc.tf
@@ -15,7 +15,7 @@ resource "aws_subnet" "main-public-1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-1a"
+  availability_zone       = "us-west-2a"
 
   tags = {
     Name = "main-public-1"
@@ -27,7 +27,7 @@ resource "aws_subnet" "main-private-1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.4.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-1a"
+  availability_zone       = "us-west-2a"
 
   tags = {
     Name = "main-private-1"
