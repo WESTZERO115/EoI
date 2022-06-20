8f555f6291108d664dff250fe3e9e336c92776de
HYERIN0718 <70850937+HYERIN0718@users.noreply.github.com>
2022-06-21 00:09:18 +0900

    Create iam-worker.tf

---
+++ Terraform,Ansible/EKS/iam-worker.tf
@@ -0,0 +1,39 @@
+resource "aws_iam_role" "tean-node" {
+  name = "terraform-eks-tean-node"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+
+}
+
+resource "aws_iam_role_policy_attachment" "tean-node-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role = aws_iam_role.tean-node.name
+}
+
+resource "aws_iam_role_policy_attachment" "tean-node-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role = aws_iam_role.tean-node.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.tean-node.name
+}
+
+resource "aws_iam_instance_profile" "tean-node" {
+  name = "terraform-eks-tean"
+  role = aws_iam_role.tean-node.name
+}
